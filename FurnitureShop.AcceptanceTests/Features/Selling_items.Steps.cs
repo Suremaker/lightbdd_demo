739dde006d9b1c24e3cd123a4ac4dc91dc6220d8
wojtek <wojciech.kotlarski@wonga.com>
2014-09-22 16:27:28 +0100

    implemented some scenarios

---
+++ FurnitureShop.AcceptanceTests/Features/Selling_items.Steps.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using LightBDD;
+
+namespace FurnitureShop.AcceptanceTests.Features
+{
+    public partial class Selling_items : FeatureFixture
+    {
+        private void Given_a_item_is_available_in_stock()
+        {
+            
+        }
+
+        private void When_order_is_placed_for_this_item()
+        {
+            Assert.Inconclusive("Not implemented yet");
+        }
+
+        private void Then_order_is_successful()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void And_item_is_removed_from_stock()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
